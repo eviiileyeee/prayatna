dacf3e97f119a226cb0695799582397782efb073
harendra <jeaturajabundela@gmail.com>
2025-03-06 07:07:22 +0530

    bb

--- server/routes/findRoutes.js
+++ server/routes/findRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const RouteController = require('../controllers/routeController');
 
 const routeController = new RouteController();
-
 router.post('/', (req, res) => routeController.findRoute(req, res));
+router.post("/port", handlePort);
 
 module.exports = router;
